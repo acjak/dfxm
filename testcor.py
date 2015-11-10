89a0da4499215cf515580a162da37bd8974a815e
Anders Jakobsen <sterrior@gmail.com>
2015-11-10 11:33:59 +0100

    Added automatic bright spot finder.

--- testcor.py
+++ testcor.py
@@ -1,6 +1,7 @@
 #!/usr/local/bin/python
 
 import numpy as np
+import scipy
 import EdfFile
 import sys
 from os import listdir
@@ -18,6 +19,24 @@ def getFilelists(filename):
 
     return data_files
 
+def findBrightSpot(img):
+    r = 10
+    spotmax = 0
+
+    # weight = np.ones((20,20))*30000.
+
+    for i in range(len(img[:,0])):
+        for j in range(len(img[0,:])):
+            if i > r and j > r and i < len(img[:,0])-r and j < len(img[0,:])-r:
+                spot_brightness = np.mean(img[i-r:i+r,j-r:j+r])
+                if spot_brightness >= spotmax:
+                    spotmax = spot_brightness
+                    maxpos = [i,j]
+
+    print spotmax, maxpos
+    print img[maxpos[0]-r:maxpos[0]+r,maxpos[1]-r:maxpos[1]+r]
+    return spotmax, maxpos
+
 data_files = getFilelists(sys.argv[1])
 
 f = EdfFile.EdfFile(data_files[0])
@@ -31,6 +50,11 @@ for fname in data_files:
     n += 1
 
 meanimg = np.mean(imgstack, 2)
+imgmin = np.min(imgstack,2)
 
-plt.imshow(np.min(imgstack,2))
+spotmax, maxpos = findBrightSpot(imgmin)
+
+plt.imshow(imgmin)
+plt.autoscale(False)
+plt.scatter(maxpos[1], maxpos[0], s=100, color='magenta')
 plt.show()
