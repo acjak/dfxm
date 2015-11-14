3732f103479026a8a08310e7a517b817997c93d5
Anders Jakobsen <sterrior@gmail.com>
2015-11-14 13:56:42 +0100

    Script to run python and MPI automatically.

---
+++ pandarun.py
@@ -0,0 +1,10 @@
+#!/usr/local/bin/python
+
+import os, sys
+
+os.system('rsync -avz /Users/andcj/PD_scripts/*.py jakobsen@panda.hpc.dtu.dk:dfxm/.')
+os.system('rsync -avz /Users/andcj/PD_scripts/lib jakobsen@panda.hpc.dtu.dk:dfxm/.')
+
+os.system('ssh jakobsen@panda.hpc.dtu.dk \"cd dfxm; mpiexec -n %s python %s\"' % (sys.argv[1], sys.argv[2]))
+
+os.system('rsync -avz jakobsen@panda.hpc.dtu.dk:dfxm/output/ /Users/andcj/PD_scripts/output/. ')
