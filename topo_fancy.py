bf0ce492e7c3903e319aa07aacdc971ae018ec8f
Anders Jakobsen <sterrior@gmail.com>
2015-12-10 14:58:11 +0100

    Updated to work with dfxm.py and output images with angle text on.

--- topo_fancy.py
+++ topo_fancy.py
@@ -3,16 +3,16 @@
 
 from lib.getedfdata import *
 from lib.gauss import *
-import numpy as np
+# import numpy as np
 
 # import matplotlib
 import matplotlib.pylab as plt
-import seaborn as sns
+# import seaborn as sns
 
-import scipy.ndimage
+# import scipy.ndimage
 import itertools
 
-import warnings
+# import warnings
 
 from mpi4py import MPI
 import time
@@ -27,11 +27,13 @@ if rank == 0:
 	start = time.time()
 
 
-path = '/data/hxrm/Dislocations_november_2015/diamond/nearfield'
+path = '/data/hxrm/Dislocations_november_2015/diamond/ff_strain'
+bg_path = '/data/hxrm/Dislocations_november_2015/diamond/bg_ff'
 
-filename = 'topotomo_nf1_'
-sampletitle = filename
-bg_filename = 'bg_1x1_1s_'
+filename = 'ff2_'
+# filename2 = 'ff2_'
+sampletitle = 'topo_fancy_txt_'
+bg_filename = 'bg_ff_2x2_0p5s_'
 
 datatype = 'topotomo'
 
@@ -42,7 +44,7 @@ datatype = 'topotomo'
 
 roi = [200, 1800, 200, 1800]
 
-data = GetEdfData(path, filename, bg_filename, roi, datatype)
+data = GetEdfData(path, filename, bg_path, bg_filename, roi, datatype, test_switch)
 
 meta = data.getMetaArray()
 hist, datx, daty = data.makeMeanGrid()
@@ -66,10 +68,15 @@ local_data = ab_vals[istart:istop]
 # 	end = time.time()
 # 	print "Init time: ", end-start
 
+fig = plt.figure()
+
 for i in range(len(local_data)):
-    index = data.getIndex(float(local_data[i][0]), float(local_data[i][1]))
-    print index, rank, local_data[i][0], local_data[i][1]
-    img = data.getImage(index[0], False)
-    plt.imshow(img, cmap='Greens', interpolation='none')
-    # plt.show()
-    plt.savefig('output/topofancy_diamond/topo_nf1_' + str("%04d" % (i+rank*local_n))+'.png') # str(i+80+rank*local_n))
+	ax.clf()
+	ax = fig.add_subplot(111)
+	index = data.getIndex(float(local_data[i][0]), float(local_data[i][1]))
+	print index, rank, local_data[i][0], local_data[i][1]
+	img = data.getImage(index[0], False)
+	ax.imshow(img, cmap='Greens', interpolation='none')
+	ax.text(5, 2, local_data[i][0])
+	# plt.show()
+	plt.savefig('output/topofancy_diamond2/topo_ff_angletext' + str("%04d" % (i+rank*local_n))+'.png')  # str(i+80+rank*local_n))
