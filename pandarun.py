1322c4e74b1cc1e6e79715edcd5f11602e968ac5
andcj <sterrior@gmail.com>
2016-04-14 12:05:59 -0700

    Added a line for running a shell script.

--- pandarun.py
+++ pandarun.py
@@ -4,7 +4,10 @@ import os, sys
 
 os.system('rsync -avz /Users/andcj/PD_scripts/*.py jakobsen@panda.hpc.dtu.dk:dfxm/.')
 os.system('rsync -avz /Users/andcj/PD_scripts/lib jakobsen@panda.hpc.dtu.dk:dfxm/.')
+os.system('rsync -avz /Users/andcj/PD_scripts/resolution_paper jakobsen@panda.hpc.dtu.dk:dfxm/.')
 
 os.system('ssh jakobsen@panda.hpc.dtu.dk \"cd dfxm; mpiexec -n %s python %s\"' % (sys.argv[1], sys.argv[2]))
+
+# os.system('ssh jakobsen@panda.hpc.dtu.dk \"cd dfxm; sh make_rec_data.sh\"')
 
 os.system('rsync -avz jakobsen@panda.hpc.dtu.dk:dfxm/output/ /Users/andcj/PD_scripts/output/. ')
