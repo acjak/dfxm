75306a76d8d57863052b7763f2614821890ef099
andcj <sterrior@gmail.com>
2016-04-14 12:08:07 -0700

    Shell script to run all reciprocal data analysis for resolution paper.

---
+++ make_rec_data.sh
@@ -0,0 +1,27 @@
+# echo "Calculating for rollscan_top"
+# mpiexec -n 8 python resolution_rec.py /data/hxrm/Resolution_march_2016/rollscan_top rollscan_top_roll ROLL
+# mpiexec -n 8 python resolution_rec.py /data/hxrm/Resolution_march_2016/rollscan_top rollscan_top_tt TT
+# echo "Calculating for rollscan_left"
+# mpiexec -n 8 python resolution_rec.py /data/hxrm/Resolution_march_2016/rollscan_left rollscan_left_roll ROLL
+# mpiexec -n 8 python resolution_rec.py /data/hxrm/Resolution_march_2016/rollscan_left rollscan_left_tt TT
+# echo "Calculating for rollscan_right"
+# mpiexec -n 8 python resolution_rec.py /data/hxrm/Resolution_march_2016/rollscan_right rollscan_right_roll ROLL
+# mpiexec -n 8 python resolution_rec.py /data/hxrm/Resolution_march_2016/rollscan_right rollscan_right_tt TT
+# echo "Calculating for rollscan_bot"
+# mpiexec -n 8 python resolution_rec.py /data/hxrm/Resolution_march_2016/rollscan_bot rollscan_bot_roll ROLL
+# mpiexec -n 8 python resolution_rec.py /data/hxrm/Resolution_march_2016/rollscan_bot rollscan_bot_tt TT
+echo "Calculating for rollscan_big"
+mpiexec -n 8 python resolution_rec.py /data/hxrm/Resolution_march_2016/rollscan_big rollscan_big_roll ROLL
+mpiexec -n 8 python resolution_rec.py /data/hxrm/Resolution_march_2016/rollscan_big rollscan_big_tt TT
+echo "Calculating for rollscan_top_far"
+mpiexec -n 8 python resolution_rec.py /data/hxrm/Resolution_march_2016/rollscan_top_far rollscan_top_far_roll ROLL
+mpiexec -n 8 python resolution_rec.py /data/hxrm/Resolution_march_2016/rollscan_top_far rollscan_top_far_tt TT
+echo "Calculating for rollscan_left_far"
+mpiexec -n 8 python resolution_rec.py /data/hxrm/Resolution_march_2016/rollscan_left_far rollscan_left_far_roll ROLL
+mpiexec -n 8 python resolution_rec.py /data/hxrm/Resolution_march_2016/rollscan_left_far rollscan_left_far_tt TT
+echo "Calculating for rollscan_right_far"
+mpiexec -n 8 python resolution_rec.py /data/hxrm/Resolution_march_2016/rollscan_right_far rollscan_right_far_roll ROLL
+mpiexec -n 8 python resolution_rec.py /data/hxrm/Resolution_march_2016/rollscan_right_far rollscan_right_far_tt TT
+echo "Calculating for rollscan_bot_far"
+mpiexec -n 8 python resolution_rec.py /data/hxrm/Resolution_march_2016/rollscan_bot_far rollscan_bot_far_roll ROLL
+mpiexec -n 8 python resolution_rec.py /data/hxrm/Resolution_march_2016/rollscan_bot_far rollscan_bot_far_tt TT
