2851f5c637084d6aa6be872f10e1c46fce8e3fc8
andcj <sterrior@gmail.com>
2016-05-01 17:52:34 -0700

    Commented out lines.

--- make_rec_data.sh
+++ make_rec_data.sh
@@ -13,15 +13,15 @@
 echo "Calculating for rollscan_big"
 mpiexec -n 8 python resolution_rec.py /data/hxrm/Resolution_march_2016/rollscan_big rollscan_big_roll ROLL
 mpiexec -n 8 python resolution_rec.py /data/hxrm/Resolution_march_2016/rollscan_big rollscan_big_tt TT
-echo "Calculating for rollscan_top_far"
-mpiexec -n 8 python resolution_rec.py /data/hxrm/Resolution_march_2016/rollscan_top_far rollscan_top_far_roll ROLL
-mpiexec -n 8 python resolution_rec.py /data/hxrm/Resolution_march_2016/rollscan_top_far rollscan_top_far_tt TT
-echo "Calculating for rollscan_left_far"
-mpiexec -n 8 python resolution_rec.py /data/hxrm/Resolution_march_2016/rollscan_left_far rollscan_left_far_roll ROLL
-mpiexec -n 8 python resolution_rec.py /data/hxrm/Resolution_march_2016/rollscan_left_far rollscan_left_far_tt TT
-echo "Calculating for rollscan_right_far"
-mpiexec -n 8 python resolution_rec.py /data/hxrm/Resolution_march_2016/rollscan_right_far rollscan_right_far_roll ROLL
-mpiexec -n 8 python resolution_rec.py /data/hxrm/Resolution_march_2016/rollscan_right_far rollscan_right_far_tt TT
-echo "Calculating for rollscan_bot_far"
-mpiexec -n 8 python resolution_rec.py /data/hxrm/Resolution_march_2016/rollscan_bot_far rollscan_bot_far_roll ROLL
-mpiexec -n 8 python resolution_rec.py /data/hxrm/Resolution_march_2016/rollscan_bot_far rollscan_bot_far_tt TT
+# echo "Calculating for rollscan_top_far"
+# mpiexec -n 8 python resolution_rec.py /data/hxrm/Resolution_march_2016/rollscan_top_far rollscan_top_far_roll ROLL
+# mpiexec -n 8 python resolution_rec.py /data/hxrm/Resolution_march_2016/rollscan_top_far rollscan_top_far_tt TT
+# echo "Calculating for rollscan_left_far"
+# mpiexec -n 8 python resolution_rec.py /data/hxrm/Resolution_march_2016/rollscan_left_far rollscan_left_far_roll ROLL
+# mpiexec -n 8 python resolution_rec.py /data/hxrm/Resolution_march_2016/rollscan_left_far rollscan_left_far_tt TT
+# echo "Calculating for rollscan_right_far"
+# mpiexec -n 8 python resolution_rec.py /data/hxrm/Resolution_march_2016/rollscan_right_far rollscan_right_far_roll ROLL
+# mpiexec -n 8 python resolution_rec.py /data/hxrm/Resolution_march_2016/rollscan_right_far rollscan_right_far_tt TT
+# echo "Calculating for rollscan_bot_far"
+# mpiexec -n 8 python resolution_rec.py /data/hxrm/Resolution_march_2016/rollscan_bot_far rollscan_bot_far_roll ROLL
+# mpiexec -n 8 python resolution_rec.py /data/hxrm/Resolution_march_2016/rollscan_bot_far rollscan_bot_far_tt TT
