2a452a3ce5efbcc2e33fdfa351a843d3555bd727
andcj <sterrior@gmail.com>
2016-05-10 13:16:53 -0700

    Only syncs .py files in resolution_paper folder.

--- pandarun.py
+++ pandarun.py
@@ -4,7 +4,7 @@ import os, sys
 
 os.system('rsync -avz /Users/andcj/PD_scripts/*.py jakobsen@panda.hpc.dtu.dk:dfxm/.')
 os.system('rsync -avz /Users/andcj/PD_scripts/lib jakobsen@panda.hpc.dtu.dk:dfxm/.')
-os.system('rsync -avz /Users/andcj/PD_scripts/resolution_paper jakobsen@panda.hpc.dtu.dk:dfxm/.')
+os.system('rsync -avz /Users/andcj/PD_scripts/resolution_paper/*.py jakobsen@panda.hpc.dtu.dk:dfxm/.')
 
 os.system('ssh jakobsen@panda.hpc.dtu.dk \"cd dfxm; mpiexec -n %s python %s\"' % (sys.argv[1], sys.argv[2]))
 
