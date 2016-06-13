6548acae25ad8a0ce20948e482f613dafa88f20d
andcj <sterrior@gmail.com>
2016-06-13 15:41:42 +0200

    .

--- make_rec_data.sh
+++ make_rec_data.sh
@@ -10,9 +10,9 @@
 # echo "Calculating for rollscan_bot"
 # mpiexec -n 8 python resolution_rec.py /data/hxrm/Resolution_march_2016/rollscan_bot rollscan_bot_roll ROLL
 # mpiexec -n 8 python resolution_rec.py /data/hxrm/Resolution_march_2016/rollscan_bot rollscan_bot_tt TT
-echo "Calculating for rollscan_big"
-mpiexec -n 8 python resolution_rec.py /data/hxrm/Resolution_march_2016/rollscan_big rollscan_big_roll ROLL
-mpiexec -n 8 python resolution_rec.py /data/hxrm/Resolution_march_2016/rollscan_big rollscan_big_tt TT
+# echo "Calculating for rollscan_big"
+# mpiexec -n 8 python resolution_rec.py /data/hxrm/Resolution_march_2016/rollscan_big rollscan_big_roll ROLL
+# mpiexec -n 8 python resolution_rec.py /data/hxrm/Resolution_march_2016/rollscan_big rollscan_big_tt TT
 # echo "Calculating for rollscan_top_far"
 # mpiexec -n 8 python resolution_rec.py /data/hxrm/Resolution_march_2016/rollscan_top_far rollscan_top_far_roll ROLL
 # mpiexec -n 8 python resolution_rec.py /data/hxrm/Resolution_march_2016/rollscan_top_far rollscan_top_far_tt TT
@@ -25,3 +25,9 @@ mpiexec -n 8 python resolution_rec.py /data/hxrm/Resolution_march_2016/rollscan_
 # echo "Calculating for rollscan_bot_far"
 # mpiexec -n 8 python resolution_rec.py /data/hxrm/Resolution_march_2016/rollscan_bot_far rollscan_bot_far_roll ROLL
 # mpiexec -n 8 python resolution_rec.py /data/hxrm/Resolution_march_2016/rollscan_bot_far rollscan_bot_far_tt TT
+
+mpiexec -n 1 python resolution_direct_line.py 300
+mpiexec -n 1 python resolution_direct_line.py 150
+mpiexec -n 1 python resolution_direct_line.py 0
+mpiexec -n 1 python resolution_direct_line.py -150
+mpiexec -n 1 python resolution_direct_line.py -300
