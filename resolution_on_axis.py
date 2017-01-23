4669a977419518bada60849aa658f6a5c2ef702f
andcj <sterrior@gmail.com>
2017-01-23 13:50:47 +0100

    Made PEP8 adjustements.

--- resolution_on_axis.py
+++ resolution_on_axis.py
@@ -43,13 +43,17 @@ datatype = 'res_paper'
 
 test_switch = True
 
-poi = [1024, 1024+500]
+poi = [1024, 1024 + 500]
 # size = [2, 20]
 size = [40, 40]
 
 pixelsize = 180.  # 180.
 
-roi = [poi[0]-size[0]/2, poi[0]+size[0]/2, poi[1]-size[1]/2, poi[1]+size[1]/2]
+roi = [
+	poi[0] - size[0] / 2,
+	poi[0] + size[0] / 2,
+	poi[1] - size[1] / 2,
+	poi[1] + size[1] / 2]
 
 data = GetEdfData(path, filename, bg_path, bg_filename, roi, datatype, test_switch)
 
@@ -69,7 +73,7 @@ a, b, c = data.getMetaValues()
 def plotImageArray(sampletitle):
 	plt.figure(figsize=(14, 14))
 	gs1 = matplotlib.gridspec.GridSpec(8, 8)
-	gs1.update(wspace=0.025,  hspace=0.03)
+	gs1.update(wspace=0.025, hspace=0.03)
 
 	for i in range(len(data.imgarray[:, 0, 0])):
 		img = data.imgarray[i, :, :]
@@ -88,20 +92,22 @@ def makeIndexList_ROLL(a, b, c):
 
 	for i in a:
 		# print rank, float(a[i]-data.alpha0), float(b[i]-data.beta0)#, i
-		index = data.getIndex(i, b[len(b)/2], c[0])
+		index = data.getIndex(i, b[len(b) / 2], c[0])
 		index_list.append(index[0])
 
 	return index_list
+
 
 def makeIndexList_TT(a, b, c):
 	index_list = []
 
 	for i in b:
 		# print rank, float(a[i]-data.alpha0), float(b[i]-data.beta0)#, i
-		index = data.getIndex(a[len(a)/2], i, c[0])
+		index = data.getIndex(a[len(a) / 2], i, c[0])
 		index_list.append(index[0])
 
 	return index_list
+
 
 def getIntList(index_list):
 	center_int = np.zeros((len(index_list)))
@@ -114,19 +120,22 @@ def getIntList(index_list):
 		data.makeImgArray(index_list, 50, 'linetrace')
 
 		for i in range(len(data.imgarray)):
-			center_int[i] = srcur_max*np.sum(data.imgarray[i])/srcur[i]
+			center_int[i] = srcur_max * np.sum(data.imgarray[i]) / srcur[i]
 		return center_int
+
 
 def fitMyShitUp(center_int, xr):
 	popt, pcov = tools.fitGaussian(xr, center_int)
 	return popt[0], popt[1], popt[2]
 
+
 def makeGaussian(xr, ampl, midp, sigma):
-	xr_fine = np.arange(min(xr), max(xr), abs(xr[1]-xr[0])/10)
+	xr_fine = np.arange(min(xr), max(xr), abs(xr[1] - xr[0]) / 10)
 	yr = []
 	for i in xr_fine:
 		yr.append(tools.gaus(i, ampl, midp, sigma))
 	return xr_fine, yr
+
 
 def convertToDegrees(b):
 	detx_specular = data.beta0
@@ -134,8 +143,8 @@ def convertToDegrees(b):
 	sample_det = 5720
 	b_degrees = []
 	for i, detx in enumerate(b):
-		b_degrees.append(math.degrees(math.atan(detx/sample_det)))
-		print i, detx, math.degrees(math.atan(detx/sample_det))
+		b_degrees.append(math.degrees(math.atan(detx / sample_det)))
+		print i, detx, math.degrees(math.atan(detx / sample_det))
 	return b_degrees
 
 index_list_tt = makeIndexList_TT(a, b, c)
@@ -144,7 +153,7 @@ index_list_roll = makeIndexList_ROLL(a, b, c)
 
 
 b_d = convertToDegrees(b)
-print b[len(b)/2], a[len(a)/2]
+print b[len(b) / 2], a[len(a) / 2]
 
 # xr_common = b_d
 
@@ -157,10 +166,10 @@ ampl_roll, midp_roll, sigma_roll = fitMyShitUp(center_int_roll, a)
 xr_tt, yr_tt = makeGaussian(b_d, ampl_tt, midp_tt, sigma_tt)
 xr_roll, yr_roll = makeGaussian(a, ampl_roll, midp_roll, sigma_roll)
 
-fwhm_tt = 2*math.sqrt(2*math.log(2))*sigma_tt
-fwhm_roll = 2*math.sqrt(2*math.log(2))*sigma_roll
+fwhm_tt = 2 * math.sqrt(2 * math.log(2)) * sigma_tt
+fwhm_roll = 2 * math.sqrt(2 * math.log(2)) * sigma_roll
 
-fig, ax = plt.subplots(figsize=(7,7))
+fig, ax = plt.subplots(figsize=(7, 7))
 # fig2, ax2 = plt.subplots(figsize=(7,7))
 ax2 = ax.twiny()
 
@@ -176,9 +185,9 @@ ln2 = ax2.plot(xr_roll, yr_roll, label=legend_roll)
 ax2.plot(a, center_int_roll, color='black', linestyle=':')
 ax2.set_xlabel('samrz [degrees]')
 
-lns = ln1+ln2
+lns = ln1 + ln2
 labs = [l.get_label() for l in lns]
-ax.legend(lns, labs, loc=0, prop={'size':8})
+ax.legend(lns, labs, loc=0, prop={'size': 8})
 ax.ticklabel_format(useOffset=False, axis='x')
 
 # ax.legend(loc='upper right', prop={'size':8})
--- resolution_rec.py
+++ resolution_rec.py
@@ -49,7 +49,11 @@ size = [2048, 2048]
 
 pixelsize = 180.  # 180.
 
-roi = [poi[0]-size[0]/2, poi[0]+size[0]/2, poi[1]-size[1]/2, poi[1]+size[1]/2]
+roi = [
+	poi[0] - size[0] / 2,
+	poi[0] + size[0] / 2,
+	poi[1] - size[1] / 2,
+	poi[1] + size[1] / 2]
 
 data = GetEdfData(path, filename, bg_path, bg_filename, roi, datatype, test_switch)
 
@@ -69,7 +73,7 @@ a, b, c = data.getMetaValues()
 def plotImageArray(sampletitle):
 	plt.figure(figsize=(14, 14))
 	gs1 = matplotlib.gridspec.GridSpec(8, 8)
-	gs1.update(wspace=0.025,  hspace=0.03)
+	gs1.update(wspace=0.025, hspace=0.03)
 
 	for i in range(len(data.imgarray[:, 0, 0])):
 		img = data.imgarray[i, :, :]
@@ -88,30 +92,32 @@ def makeIndexList_ROLL(a, b, c):
 
 	for i in a:
 		# print rank, float(a[i]-data.alpha0), float(b[i]-data.beta0)#, i
-		index = data.getIndex(i, b[len(b)/2], c[0])
+		index = data.getIndex(i, b[len(b) / 2], c[0])
 		index_list.append(index[0])
 
-	xr = a-data.alpha0
+	xr = a - data.alpha0
 
 	with warnings.catch_warnings():
 		warnings.simplefilter("ignore")
 		data.makeImgArray(index_list, 50, 'linetrace')
 		return tools.makeNewGaussArrayMPI(data.imgarray, 1, xr), index_list
+
 
 def makeIndexList_TT(a, b, c):
 	index_list = []
 
 	for i in b:
 		# print rank, float(a[i]-data.alpha0), float(b[i]-data.beta0)#, i
-		index = data.getIndex(a[len(a)/2], i, c[0])
+		index = data.getIndex(a[len(a) / 2], i, c[0])
 		index_list.append(index[0])
 
-	xr = b-data.beta0
+	xr = b - data.beta0
 
 	with warnings.catch_warnings():
 		warnings.simplefilter("ignore")
 		data.makeImgArray(index_list, 50, 'linetrace')
 		return tools.makeNewGaussArrayMPI(data.imgarray, 1, xr), index_list
+
 
 def saveGaussArray(sampletitle, gaussarray):
 	np.save(data.directory + '/' + sampletitle + '_data.npy', gaussarray)
@@ -131,4 +137,4 @@ if rank == 0:
 		# tools.plotStrain(strainpic, data.imgarray)
 
 	end = time.time()
-	print "Time:", end-start, "seconds."
+	print "Time:", end - start, "seconds."
