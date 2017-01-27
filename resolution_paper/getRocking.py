46ae98876128b91a99e9b50f56984a3d1edce2f4
andcj <sterrior@gmail.com>
2017-01-27 11:32:21 +0100

    PEP8 adjustments.

--- resolution_paper/getRocking.py
+++ resolution_paper/getRocking.py
@@ -14,9 +14,9 @@ import sys
 import warnings
 
 
-
 def gaus(x, a, x0, sigma):
-	return a*np.exp(-(x-x0)**2/(2*sigma**2))
+	return a * np.exp(-(x - x0)**2 / (2 * sigma**2))
+
 
 def fitGaussian(x, y):
 	from scipy.optimize import curve_fit
@@ -32,8 +32,9 @@ def fitMyShitUp(center_int, xr):
 	popt, pcov = fitGaussian(xr, center_int)
 	return popt[0], popt[1], popt[2]
 
+
 def makeGaussian(xr, ampl, midp, sigma):
-	xr_fine = np.arange(min(xr), max(xr), abs(xr[1]-xr[0])/10)
+	xr_fine = np.arange(min(xr), max(xr), abs(xr[1] - xr[0]) / 10)
 	yr = []
 	for i in xr_fine:
 		yr.append(gaus(i, ampl, midp, sigma))
@@ -43,41 +44,37 @@ def makeGaussian(xr, ampl, midp, sigma):
 BG = 102.
 scan1 = np.loadtxt('scan524.txt')
 
-diffrz = scan1[:,0]
-ccdavg = scan1[:,7]
+diffrz = scan1[:, 0]
+ccdavg = scan1[:, 7]
 
-pop1, pop2, pop3 = fitMyShitUp(ccdavg-BG, diffrz)
+pop1, pop2, pop3 = fitMyShitUp(ccdavg - BG, diffrz)
 
 xr_fine, yr = makeGaussian(diffrz, pop1, pop2, pop3)
 
 
 # mpy = len(detnorm[0,:])/2
 # mpx = len(detnorm[:,0])/2
-fwhm = 2*math.sqrt(2*math.log(2))*pop3
-ampl = (pop3*math.sqrt(2*math.pi))/pop1
+fwhm = 2 * math.sqrt(2 * math.log(2)) * pop3
+ampl = (pop3 * math.sqrt(2 * math.pi)) / pop1
 
 
 sns.set_style('white')
-fig, ax = plt.subplots(figsize=(7,7))
+fig, ax = plt.subplots(figsize=(7, 7))
 # fig2, ax2 = plt.subplots(figsize=(7,7))
 # ax2 = ax.twiny()
 
 
 legend_tt = 'Ampl:' + str(pop1) + '\n Midp:' + str(pop2) + '\n FWHM:' + str(fwhm)
 
-ax.plot(diffrz, ccdavg-BG)
+ax.plot(diffrz, ccdavg - BG)
 ax.plot(xr_fine, yr, label=legend_tt)
-ax.legend(prop={'size':10})
+ax.legend(prop={'size': 10})
 ax.ticklabel_format(useOffset=False, axis='x')
 ax.set_title('Rocking curve, diamond, resolution experiment')
 ax.set_xlabel('diffrz [degrees]')
 ax.set_ylabel('counts (BG subtracted)')
 
-
-
 fig.savefig('rockingcurve.png')
-
-
 
 # legend_roll = 'Roll Ampl:' + str(ampl_roll) + '\n Roll Midp:' + str(midp_roll) + '\n Roll FWHM:' + str(fwhm_roll)
 #
